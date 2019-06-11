3ce03541506fd3f955a181a9984bfd2df153c73e
miguel <miguel.robles@atmosfera.unam.mx>
2019-06-11 16:29:19 -0500

    2 plots, add mul by zen_cos

--- add_time.py
+++ add_time.py
@@ -4,11 +4,15 @@ import glob
 import os
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
+import sys
+import datetime as dt
+from matplotlib.dates import DateFormatter
+import solarPos as solar
 
-
-myfiles = [] #list of image filenames
-origin = input('Introduce ruta de la carpeta de origen.\nPor ejemplo: '
-    'C:/Users/Invitado/Documentos/log_22nov17_105900\n ')
+sol=solar.solarPos(19.4, -99.149999, -6)
+origin=sys.argv[1]
+col_var=2
+col_err=3
 root=origin.split("/")[-1]
 root=root.split("_")[0]
 print(root)
@@ -28,38 +32,78 @@ if not os.path.isdir(origin):
 hours = []
 filenames=glob.glob(os.path.join(origin, '*.dat'))
 filenames.sort() # inicia ordena pero no ordena numericamente bien
+ifmt="%y%m%d_%H%M%S"
 for i,name in enumerate(filenames):
-    tname=name.split("_")[-1]
-    filename=tname.split(".")[0]
-    # Cambiar a formato XXhYYmZZs
-    hour = filename[:2] + ':' + filename[2:4] + ':' + filename[4:]
-    hours.append(hour)
+    filename=name.split("/")[-1].split('.')[0]
+    hours.append(dt.datetime.strptime(filename,ifmt))
 
 #print(hours)
 #print(filenames)
 # graficar NO2_425-490.SlCol(NO2) y NO2_425-490.SlErr(NO2)
 # eliminar negativos por nan
 
+head='date\t'
 with open(output, 'w') as outfile:
     for i,line in enumerate(open(ruta)) :
+        #get data
+        data=line.split('\t')
+        if i==1:
+            head+=data[col_var]+'\t'+data[col_err]
+            print(head,file=outfile)
         if i>1:
-            #add time to data ! erase here?
-            print(hours[i-2],line[:-2],sep="\t", file=outfile)
+            #add time & extract data cols  
+            print(dt.datetime.strftime(hours[i-2],ifmt),
+                data[col_var],data[col_err],
+                sep="\t", 
+                file=outfile,
+                )
 
-hms, no2, e_no2 = [], [], []
+hms=np.array([])
+no2=np.array([])
+err=np.array([])
+s_pos=np.array([])
+plot_fmt = DateFormatter("%H:%M") 
 with open(output, 'r') as f:
-    for line in f:
-        #read no2_val
-        no2_val = float(line.split('\t')[3]) / 1e17
-        if no2_val > 0:
-            hms.append(line.split('\t')[0])
-            no2.append(no2_val)
-            e_no2.append(float(line.split('\t')[2]) / 1e17)
+    for i,line in enumerate(f):
+        if i==0:
+            continue
+        h=dt.datetime.strptime(line.split('\t')[0],ifmt)
+        hms=np.append(hms, h)
+        sol.calculate(h)
+        s_pos=np.append(s_pos,sol.zen_cos)
+        no2_val = float(line.split('\t')[1])
+        no2=np.append(no2,no2_val)
+        err=np.append(err,float(line.split('\t')[2]))
 
-fig, ax = plt.subplots(1,1)
-plt.plot(hms, no2, lw=2)
-plt.errorbar(hms, no2, yerr=e_no2, linestyle='None', lw=.7)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
-plt.xticks(rotation=45)
-plt.ylabel("No2.SlCol(NO2) (/1e17)")
+fig, ax = plt.subplots(2)
+ax[0].plot(hms,no2)
+no2_max=np.max(no2)
+ax[0].plot(hms,s_pos*no2_max)
+subs=15
+ax[0].errorbar(hms[::subs], no2[::subs],
+        yerr=err[::subs],
+        fmt='gray',
+        capsize=5,
+        linestyle='None',
+        #lw=.7,
+        )
+ax[1].plot(hms,s_pos*no2)
+ax[1].errorbar(
+        hms[::subs],
+        no2[::subs]*s_pos[::subs],
+        yerr=err[::subs]*s_pos[::subs],
+        fmt='gray',
+        capsize=5,
+        linestyle='None',
+        #lw=.7,
+        )
+for a in ax:
+    a.label_outer()
+lims=list(plt.axis())
+#lims[2]=0
+#plt.axis(lims)
+#plt.xticks(rotation=45)
+ax[1].xaxis.set_major_formatter(plot_fmt)
+#plt.ylabel(head.split('\t')[1])
+fig.suptitle(head.split('\t')[1])
 plt.show()
